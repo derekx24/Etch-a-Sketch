7c1ee1952dd14e4af68ee79ca0fa223793febf47
Derek Xu <derek24@students.cs.ubc.ca>
2023-03-18 23:14:14 -0700

    add slider to choose pixel size

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-function createGrid () {
-    grid.style.gridTemplateRows = `repeat(16, 1fr)`;
-    grid.style.gridTemplateColumns = `repeat(16, 1fr)`;
+let GRID_SIZE = 16;
 
-    for(let i = 0; i < 256; i++) {
+const grid = document.getElementById('grid');
+const slider = document.getElementById('size-slider');
+const value = document.getElementById('value');
+const position = document.getElementById('position');
+
+slider.onchange = (e) => updateSize(e.target.value);
+slider.onmousemove = (e) => updateValue(e.target.value);
+
+function reloadGrid() {
+    grid.innerHTML = '';
+    createGrid(GRID_SIZE);
+}
+
+function updateSize(v) {
+    GRID_SIZE = v;
+    reloadGrid();
+}
+
+function updateValue(v) {
+    position.style.width = `${v}%`;
+    value.innerHTML = `${v} x ${v}`
+}
+
+function createGrid (v) {
+    grid.style.gridTemplateRows = `repeat(${v}), 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${v}, 1fr)`;
+
+    for(let i = 0; i < v*v; i++) {
         const gridElement = document.createElement('div');
         gridElement.classList.add("grid-element");
         gridElement.addEventListener('mouseover', changeColor = (e) => {
-            e.target.style.backgroundColor = '#ffffff';
+            e.target.style.backgroundColor = `#000000`;
         });
         grid.appendChild(gridElement);
     }
 }
 
 window.onload = () => {
-    createGrid();
+    createGrid(GRID_SIZE);
 }
